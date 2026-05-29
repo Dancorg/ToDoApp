b94014ef83167cc6f4105cdd86bcc37a876794f3
Dan Bas <daniel.issab@gmail.com>
2026-05-29 12:11:05 -0300

    Updated playwright config for app starting

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,8 +75,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-     command: 'npm run start',
-     url: 'http://localhost:3001',
-     reuseExistingServer: !process.env.CI,
-   },
+    command: 'npm run dev --prefix app',
+    url: process.env.BASE_URL!,
+    reuseExistingServer: !process.env.CI,
+  },
 });
